c758d7d9c2cab2a176d28888b32817e06fb93efd
Punk <jump.e.r@yandex.ru>
2016-05-16 00:05:43 +0300

    First commit

---
+++ Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace jumper423\module\bonus;
+
+use Yii;
+
+class Module extends \yii\base\Module
+{
+    /**
+     * @var string The controller namespace to use
+     */
+    public $controllerNamespace = 'jumper423\module\bonus\controllers';
+
+    /**
+     * Init module
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
