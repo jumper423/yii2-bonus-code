178c3e16f9e1eda160a880c338074f7562ed5282
Punk <jump.e.r@yandex.ru>
2016-05-16 00:46:18 +0300

    Migrate

---
+++ migrations/m140618_045255_create_settings.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Class m160516_003255_create_bonus
+ */
+class m160516_003255_create_bonus extends \yii\db\Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            '{{%bonus}}',
+            [
+                'id' => $this->primaryKey(),
+                'series' => $this->string(64)->notNull(),
+                'number' => $this->integer()->notNull()->unique(),
+                'generate_at' => $this->integer()->notNull(),
+                'completion_at' => $this->integer()->notNull(),
+                'use_at' => $this->integer(),
+                'active' => $this->boolean()->defaultValue(true)->notNull(),
+            ]
+        );
+        $this->createIndex('bonus_number_INDEX', '{{%bonus}}', 'number', true);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%bonus}}');
+    }
+}
